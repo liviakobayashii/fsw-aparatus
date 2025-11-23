9e603dfa152d78eba393802169b0c9561fd91409
Livia Kobayashi <liviakmmartins@gmail.com>
2025-11-23 10:53:53 -0300

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/app/_components/header";
 import SearchInput from "./_components/search-input";
 import Image from "next/image";
 import banner from "@/public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 export default function Home() {
   return (
@@ -14,6 +15,15 @@ export default function Home() {
           alt="Agende agora!"
           sizes="100vw"
           className="h-auto w-full"
+        />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="Corte de cabelo"
+          barbershopName="Barbearia do João"
+          barbershopImageUrl="https://utfs.io/f/6b0888f8-b69f-4be7-a13b-52d1c0c9cab2-17m.png"
+          date={new Date()}
         />
       </div>
     </div>
