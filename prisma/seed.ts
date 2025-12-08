9ad04e4dd5308fca889fabb102a9d1c4541365e9
Livia Kobayashi <liviakmmartins@gmail.com>
2025-12-08 10:22:35 -0300

    chore: system settings

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 async function seedDatabase() {
   try {
